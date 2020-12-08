280a7a9b2b99fe9a95756be7c6a861a6d81fce38
Peter Gemmell <peter.gemmell@ocsbstudent.ca>
2020-12-08 00:07:29 +0000

    h

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function myFunction() {
   document.getElementById("demo").style.color = "blue";
+  document.getElementById("demo").style.fontSize = "200%";
+  document.getElementById("demo").style.fontFamily = "verdana"
 }
+
